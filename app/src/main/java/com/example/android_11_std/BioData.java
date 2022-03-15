6d4c7ef1c615a33f5ec5c5a0e8c438c99fb0d4e8
rnw <rnwprogrammingvidit@gmail.com>
2022-03-15 12:00:02 +0530

    15-03-2022

---
+++ app/src/main/java/com/example/android_11_std/BioData.java
@@ -0,0 +1,14 @@
+package com.example.android_11_std;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BioData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bio_data);
+    }
+}
